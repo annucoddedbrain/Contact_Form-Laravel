2c5d733a69d70deb87050d3a2e51424849b56bc1
AzzadCodeBrain <azzad@coddedbrain.com>
2023-07-03 14:29:05 +0530

    making route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MemberController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +20,4 @@ Route::get('/', function () {
 
 
 Route::view('add', 'addmember');
+Route::post('add', [MemberController::class, 'addData']);
